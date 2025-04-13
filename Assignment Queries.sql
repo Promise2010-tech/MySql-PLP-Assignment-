6d31f2cf8d3960dc71ce53ddf405f081f1ca5530
Promise Sibanda <brilliantpromise0@gmail.com>
2025-04-13 21:11:47 +0200

    Add files via upload

---
+++ Assignment Queries.sql
@@ -0,0 +1,5 @@
+create table customer_address (
+customer_address_ID int primary key,
+customer_address varchar (100),
+-- to allow customers to input more than one address we need to ensure a flexible relationship
+);
